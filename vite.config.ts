aadcf79a1586ab8dbeb26176bc16d98dcaea4bac
shohamD1121 <shohamdouek@gmail.com>
2022-11-26 19:54:21 +0200

    upadte

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/Secyoo/",
+  plugins: [react()],
+});
+
+// How to update
+// npm run build
+// git add dist -f
+// git commit -m "updated dist"
+// git subtree push --prefix dist origin gh-pages
