334e725836a48f2173d6578db5003be24b38e902
shohamD1121 <shohamdouek@gmail.com>
2022-11-28 20:48:10 +0200

    updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 import logo from "../../images/logo.png";
@@ -6,6 +6,7 @@ import { routes } from "../../routes/routes";
 
 const Navbar: React.FC = () => {
   const { width } = useWindowDimensions();
+  const [isOpen, setIsOpen] = useState(false);
 
   return width > 768 ? (
     <nav className="flex justify-center w-full bg-cyan-100 h-[72px]">
@@ -39,54 +40,72 @@ const Navbar: React.FC = () => {
       </div>
     </nav>
   ) : (
-    <div className="bg-cyan-100">
-      <div className="flex items-center justify-between py-5">
-        <Link to="/">
-          <img src={logo} alt="skilty-logo" className="w-1/3" />
-        </Link>
+    <div className="flex justify-between items-center w-full bg-cyan-100 h-[72px]">
+      <img src={logo} alt="logo" className="w-1/3" />
+      <div
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex flex-col justify-center items-center w-1/6 h-4/5 mr-2"
+      >
+        <span
+          className={`bg-blue-500  h-[2px] block w-full ease-in-out duration-300 ${
+            !isOpen ? "translate-y-[-0.75rem]" : "rotate-[-45deg]"
+          }`}
+        ></span>
+        <span
+          className={`bg-blue-500  h-[2px] block w-full ease-in-out duration-[400ms]   ${
+            !isOpen ? "" : "opacity-0"
+          }`}
+        ></span>
+        <span
+          className={`bg-blue-500  h-[2px] block w-full ease-in-out duration-300 ${
+            !isOpen ? "translate-y-3" : "translate-y-[-4px] rotate-[45deg]"
+          }`}
+        ></span>
       </div>
-      <label className="ham-container">
-        <input type="checkbox" />
-        <span className="menu">
-          <span className="hamburger"></span>
-        </span>
-        <ul className="flex flex-col justify-center w-1/2 h-1/2 text-2xl">
-          <li>
-            <Link
-              className="mb-[1em] block no-underline text-blue-400 text-center"
-              to={routes.MAIN}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="mb-[1em] block no-underline text-blue-400 text-center"
-              to={routes.MAIN}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="mb-[1em] block no-underline text-blue-400 text-center"
-              to={routes.MAIN}
-            >
-              Contact us
-            </Link>
-          </li>
-          <li>
-            <div className="space-x-2 md:inline-block">
+      <div
+        className={`flex  justify-center items-center bg-cyan-100 absolute top-[72px] ease-in-out duration-500 w-full h-[calc(100vh-72px)] right-0 z-10 ${
+          !isOpen ? "translate-x-full" : ""
+        }`}
+      >
+        <div className="flex flex-col h-5/6 w-3/4">
+          <ul>
+            <li>
               <Link
+                className="mb-[1em] block no-underline text-blue-700 text-center hover:scale-[1.2]"
                 to={routes.MAIN}
-                className="block text-blue-400 px-4 py-2 bg-cyan-100 rounded-xl text-center"
               >
-                Join us
+                Home
               </Link>
-            </div>
-          </li>
-        </ul>
-      </label>
+            </li>
+            <li>
+              <Link
+                className="mb-[1em] block no-underline text-blue-700 text-center hover:scale-[1.2]"
+                to={routes.MAIN}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="mb-[1em] block no-underline text-blue-700 text-center hover:scale-[1.2]"
+                to={routes.MAIN}
+              >
+                Contact us
+              </Link>
+            </li>
+            <li>
+              <div className="">
+                <Link
+                  to={routes.MAIN}
+                  className="block w-3/4 mx-auto text-blue-700 px-2 py-1 bg-cyan-200 rounded-xl text-center hover:scale-[1.1]"
+                >
+                  Join us
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
