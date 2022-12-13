725f26876d61a735aee7c798a4bc2116a5e11eeb
shohamD1121 <shohamdouek@gmail.com>
2022-12-13 23:02:16 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://www.secyoo.com",
+  base: "https://www.secyoo.com/",
   plugins: [react()],
 });
 
